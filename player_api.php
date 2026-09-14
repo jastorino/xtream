832669ebe049caefceaa22fe1c8d1e05a400fe7c
jastorino <john@astorino.net>
2026-09-13 21:55:57 -0400

    Update player_api.php

--- player_api.php
+++ player_api.php
@@ -26,7 +26,7 @@ if ($action == 'get_live_streams') {
             "stream_id" => 100,
             "name" => "My Man godfrey",
             "stream_type" => "movie",
-            "container_extension" => "mov",
+            "container_extension" => "mp4",
             "stream_icon" => "https://images.justwatch.com/poster/35151046/s332/my-man-godfrey.avif",
             "direct_source" => "<unsafe_url>https://dn600307.us.archive.org/0/items/MyManGodfrey1936/MyManGodfrey1936_512kb.mp4</unsafe_url>"
         ]
