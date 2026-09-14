2da0bd9d145a2d9926acd6e15b1d55d193aae12b
jastorino <john@astorino.net>
2026-09-13 22:02:21 -0400

    Update player_api.php

--- player_api.php
+++ player_api.php
@@ -28,7 +28,10 @@ if ($action == 'get_live_streams') {
             "stream_type" => "movie",
             "container_extension" => "mp4",
             "stream_icon" => "https://images.justwatch.com/poster/35151046/s332/my-man-godfrey.avif",
-            "direct_source" => "https://dn600307.us.archive.org/0/items/MyManGodfrey1936/MyManGodfrey1936_512kb.mp4"
+            "direct_source" => "",
+            "movie_data" => [
+                "stream_url" => "https://archive.org/download/MyManGodfrey1936/MyManGodfrey1936_512kb.mp4"
+            ]
         ]
     ]);
 } elseif ($action == 'get_vod_info') {
