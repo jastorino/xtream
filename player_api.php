a0bffa12795e4388792ccb9edd55b0496fb3e729
jastorino <john@astorino.net>
2026-09-14 09:47:21 -0400

    Update player_api.php

--- player_api.php
+++ player_api.php
@@ -32,7 +32,15 @@ if ($action == 'get_live_streams') {
             "name" => "DW English",
             "stream_type" => "live",
             "stream_icon" => "https://upload.wikimedia.org/wikipedia/commons/9/93/Deutsche_Welle_logo.svg",
-            "direct_source" => "https://dwamdstream102.akamaized.net/hls/live/2015525/dwstream102/master.m3u8"
+            "direct_source" => "<unsafe_url>https://dwamdstream102.akamaized.net/hls/live/2015525/dwstream102/master.m3u8</unsafe_url>"
+        ],
+        [
+            "category_id" => "1000",
+            "stream_id" => 4,
+            "name" => "France 24 English",
+            "stream_type" => "live",
+            "stream_icon" => "<unsafe_url>https://upload.wikimedia.org/wikipedia/commons/b/b5/France_24_logo.svg</unsafe_url>",
+            "direct_source" => "<unsafe_url>https://f24hls-i.akamaihd.net/hls/live/221193/F24_EN_LO_HLS/master.m3u8</unsafe_url>"
         ]
     ]);
 } elseif ($action == 'get_vod_streams') {
