ee9c4e43b66942b71801b8c7d77b50acc8eabd9f
jastorino <john@astorino.net>
2026-09-13 20:59:41 -0400

    Update player_api.php

--- player_api.php
+++ player_api.php
@@ -27,7 +27,7 @@ if ($action == 'get_live_streams') {
             "name" => "My Man godfrey",
             "stream_type" => "movie",
             "container_extension" => "mp4",
-            "stream_icon" => "https://images.justwatch.com/poster/35151046/s332/my-man-godfrey.avif"
+            "stream_icon" => "https://images.justwatch.com/poster/35151046/s332/my-man-godfrey.avif",
             "direct_source" => "<unsafe_url>https://cdndirector.dailymotion.com/cdn/manifest/video/x99kkr2.m3u8?sec=a9Dp7tJ1wevzR6lRUuBXF2o9XBXXhu28vyPJT-K6cVy_-uo5_jflVqfJMnkyErgw-AVVNg9K6QifdRSY7pUkLE6fRNDYssnIbLjxqszVPQU</unsafe_url>",
             "info" => [
                 "plot" => "Fifth Avenue socialite Irene Bullock needs a "forgotten man" to win a scavenger hunt, and no one is more forgotten than Godfrey Park, who resides in a dump by the East River. Irene hires Godfrey as a servant for her riotously unhinged family, to the chagrin of her spoiled sister, Cornelia, who tries her best to get Godfrey fired. As Irene falls for her new butler, Godfrey turns the tables and teaches the frivolous Bullocks a lesson or two.",
